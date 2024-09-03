ea83ebee1b7f178349f2b973b95810831337dadb
JesusMartinezRuano <jesus.martinez.ruano@gmail.com>
2024-09-03 08:35:53 +0200

    Add files via upload

---
+++ preset01.py
@@ -0,0 +1,8 @@
+import requests
+
+s = requests.Session()
+s.get('http://192.168.0.178/cgi-bin/MUH44TP_getsetparams.cgi',auth=('admin','admin'))
+
+print (s.cookies)
+
+s.post('http://192.168.0.178/cgi-bin/MMX32_Keyvalue.cgi',auth=('admin','admin'),data='{CMD=PresetRecall01.')
